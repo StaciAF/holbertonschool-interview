c2e68b904b8735beaceb521e7803c003a7284506
StaciAF <aaen.it19@gmail.com>
2020-12-10 19:21:04 -0600

    adds doc string before function

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""
+given n number of boxes, each box numbered sequentially (indexed),
+each box may contain keys to itself or other boxes
+"""
 
 
 def canUnlockAll(boxes):
     keyL = boxes[0]
     open = [0]
+
     for key in keyL:
-        open.append(key)
-        keyL.extend(boxes[key])
+        if key not in open:
+            open.append(key)
+            keyL.extend(boxes[key])
+
     if len(boxes) == len(open):
         return True
     else:
