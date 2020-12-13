c065c164e466a2dd2351baf4c8ae0969401a6cfa
StaciAF <aaen.it19@gmail.com>
2020-12-13 12:22:39 -0600

    removed added test to reflect original test case given

--- 0x00-lockboxes/tests/main_0.py
+++ 0x00-lockboxes/tests/main_0.py
@@ -10,15 +10,3 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
-
-boxes = [[4, 0, 3], [6, 2], [1], [5, 9], [0, 0, 0], [8]]
-print(canUnlockAll(boxes))
-
-boxes = 3
-print(canUnlockAll(boxes))
-
-boxes = [1, 4, 5, 6]
-print(canUnlockAll(boxes))
-
-boxes = [[]]
-print(canUnlockAll(boxes))
