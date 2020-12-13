322711d9265150aa10ce4aade519a439bdbda436
StaciAF <aaen.it19@gmail.com>
2020-12-13 12:21:09 -0600

    removes extraneous try/except blocks

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -21,20 +21,15 @@ def canUnlockAll(boxes):
         True if all boxes can be unlocked
         False if all boxes can not be unlocked
     """
-
-    if type(boxes) is not list:
-        return False
-
     keyL = boxes[0]
     open = [0]
     for key in keyL:
         if key not in open:
             try:
-                open.append(key)
                 keyL.extend(boxes[key])
+                open.append(key)
             except:
                 continue
-
     if len(boxes) == len(open):
         return True
     return False
