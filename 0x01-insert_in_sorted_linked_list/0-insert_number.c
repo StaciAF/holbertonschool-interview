7d60aae08e7bbfa289e6eb185acff4fdcade179e
StaciAF <aaen.it19@gmail.com>
2020-12-16 12:27:02 -0600

    adds solution to task, format checked, compiled with error flags - updated lists.h to include library for malloc

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -5,32 +5,32 @@
  * @head: pointer to linked list
  * @number: integer value to be inserted with new node
  * Return: address of new node or NULL on failure
-*/
+ */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    if (number < 0)
-        return (NULL);
+	listint_t *new_node = NULL;
+	listint_t *current = NULL;
 
-    listint_t *new_node = NULL;
-    listint_t *current = NULL;
+	if (number < 0)
+		return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
-    new_node->n = number;
-    if (*head == NULL)
-    {
-        *head = new_node;
-        new_node->next = NULL;
-        return (new_node);
-    }
-    current = *head;
-    while (current->next != NULL && number >= current->next->n)
-    {
-        current = current->next;
-    }
-    new_node->next = current->next;
-    current->next = new_node;
-    return (new_node);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	current = *head;
+	while (current->next != NULL && number >= current->next->n)
+	{
+		current = current->next;
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
