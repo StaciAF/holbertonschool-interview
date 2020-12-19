37a2e4b35d87380ad34d75c66fb275b28df11824
StaciAF <aaen.it19@gmail.com>
2020-12-19 16:18:21 -0600

    adds README.md, header file, and file structure for project

---
+++ 0x02-heap_insert/binary_trees.h
@@ -0,0 +1,25 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREE_H
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s heap_t;
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
