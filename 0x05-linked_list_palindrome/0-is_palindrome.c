76e6cb7b5a9a8feff0ed364d4ff9caba47fd7062
StaciAF <aaen.it19@gmail.com>
2021-01-20 23:48:17 +0000

    updates formatting to betty styling

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -3,46 +3,46 @@
 /**
  * is_palindrome - checks if singly linked list is a palindrom
  * @head: head of given singly linked list
- * 
- * Return - 1 for palindrome, 0 if not
+ *
+ * Return: 1 for palindrome, 0 if not
  */
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *current;
-    int list_size = 0;
-    int compare_array[4096];
-    int i = 0;
+	listint_t *current;
+	int list_size = 0;
+	int compare_array[4096];
+	int i = 0;
 
 
-    if (head == NULL) 
-    {
-        return(0);
-    }
-    // finds size of linked list
-    current = *head;
-    while (current != NULL)
-    {
-        compare_array[list_size] = current->n;
-        list_size++;
-        current = current->next;
-    }
-    if (list_size < 2)
-    {
-        return (0);
-    }
-    current = *head;
-    while (i < list_size)
-    {
-        if (compare_array[i] == current->n)
-        {
-            i++;
-            current = current->next;
-        }
-        else
-        {
-            return (0);
-        }
-    }
-    return (1);
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	current = *head;
+	while (current != NULL)
+	{
+		compare_array[list_size] = current->n;
+		list_size++;
+		current = current->next;
+	}
+	if (list_size < 2)
+	{
+		return (0);
+	}
+	current = *head;
+	while (i < list_size)
+	{
+		if (compare_array[i] == current->n)
+		{
+			i++;
+			current = current->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
