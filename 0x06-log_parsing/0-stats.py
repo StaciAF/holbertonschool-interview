0ad141a9ba970f519cc1caeed0378e084b0d6cc4
StaciAF <aaen.it19@gmail.com>
2021-01-28 17:22:08 -0600

    prints status codes, count incorrect, needs file_size

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+this module includes a script that reads stdin
+line by line and computes requested metrics
+"""
+
+import fileinput
+import re
+
+regex = '\s+\d+\s+'
+count = 0
+stat_list = []
+
+for line in fileinput.input():
+    count += 1
+    print(line)
+    tup_status = re.findall(regex, line)
+    status_code = ''.join(tup_status)
+    status_code = status_code.strip()
+    stat_list.append(status_code)
+    stat_list = set(stat_list)
+    stat_list = list(stat_list)
+    stat_list.sort()
+    if count == 10:
+        for status in stat_list:
+            print(status + ': ' + str(stat_list.count(status)))
+        count = 0
+        stat_list = []
