2704fc7a208fb20ba96212b2953e67fed996be65
StaciAF <aaen.it19@gmail.com>
2021-02-02 17:36:24 +0000

    updates files wit betty linter styling

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -6,28 +6,19 @@
  * Return: 1 (true) if cycle exists else 0 (false)
  */
 
-int check_cycle(listint_t *list) {
+int check_cycle(listint_t *list)
+{
+	if (list == NULL)
+		return (0);
+	listint_t *slow = list;
+	listint_t *fast = list->next;
 
-    // checking addresses not values  -- does slow == fast
-    // check if head is null return 0
-    if list == NULL
-        return (0);
-    // slow mover = head - start at beginning
-    listint_t *slow = list;
-    // fast mover = head->next - start in front of slow
-    listint_t *fast = list->next;
-    // while slow != fast (move both) -- keep moving as long as the two dont equal each other (slow == fast)
-    while (slow != fast)
-    {
-        // if fast or fast->next == null -- reached end of list
-            // return 0 -- not a cycle
-        if (fast == NULL || fast->next == NULL)
-            return (0);
-        // slow = slow->next (one at a time)
-        // fast = fast->next->next (two at a time)
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    // return 1 -- is a cycle
-    return (1); 
+	while (slow != fast)
+	{
+		if (fast == NULL || fast->next == NULL)
+			return (0);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (1);
 }
