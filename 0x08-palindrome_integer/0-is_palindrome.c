83770d4d85336be7b7b7923f772615816c27fc63
StaciAF <aaen.it19@gmail.com>
2021-02-11 17:57:39 +0000

    updated function logic to manage efficiency check

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,36 +1,25 @@
 #include "palindrome.h"
 
 /**
- * is_palindrome - Function to check whether given unsigned int is a palindrome
- * @n: unsigned int to be checked as palindrome
- * Return: 1 for palindrome, 0 otherwise
+ * is_palindrome - checks if unsigned int passed is a palindrome
+ * @n: unsigned int passed to be checked as palindrome
+ * Return: 1 if palindrome, otherwise 0
  */
 
 int is_palindrome(unsigned long n)
 {
-	unsigned long first_divisor = 1000000000000000000;
-	unsigned long first_digit = n / first_divisor;
-	unsigned long last_divisor = 10, last_digit;
+	unsigned int remainder, reverse = 0, original;
 
-	if (n < 10)
+	original = n;
+	while (n != 0)
 	{
-		return (1);
-	}
-	while (first_digit == 0)
-	{
-		first_divisor /= 10;
-		first_digit = n / first_divisor;
-	}
-	last_digit = n % last_divisor;
-	while (first_digit == last_digit)
-	{
-		first_digit = n / first_divisor;
-		last_digit = n % last_divisor;
-		n -= first_divisor * first_digit;
+		remainder = n % 10;
+		reverse = reverse * 10 + remainder;
 		n /= 10;
-		first_divisor /= 100;
-		if (first_divisor < last_divisor)
-			return (1);
 	}
+
+	if (original == reverse)
+		return (1);
+
 	return (0);
 }
