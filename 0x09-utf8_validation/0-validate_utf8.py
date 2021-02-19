19d0cc48dd957fa43c52d12f85b77b1be7e6a90e
StaciAF <aaen.it19@gmail.com>
2021-02-19 11:45:40 -0600

    updates project with string manipulation approach to validating UTF-8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+def validUTF8(data):
+    # iterate over each integer in list
+    numBytes = 0
+
+    for each in data:
+        # get least 8 bits
+        binaryForm = format(each, '#010b')[-8:]
+        if numBytes == 0:
+            # how many 1s in beginning
+            for bit in binaryForm:
+                if bit == '0':
+                    break
+                numBytes += 1
+
+            if numBytes == 0:
+                continue
+            # can only be 1-4 bytes
+            if numBytes == 1 or numBytes > 4:
+                return False
+        else:
+            if not (binaryForm[0] == '1' and binaryForm[1] == '0'):
+                return False
+        numBytes -= 1
+
+    return numBytes == 0
