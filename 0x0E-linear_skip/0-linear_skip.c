18e2078e60e0128d8c68f7080fa824c02dee2b9e
StaciAF <aaen.it19@gmail.com>
2021-03-31 16:33:22 -0500

    adds imcomplete solution to algorithm

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,45 @@
+#include "search.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of ints
+ * @list: pointer to the head of the skip list
+ * @value: the value to search for
+ * Return: a pointer to the first node of value or NULL
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *myEx = NULL;
+    skiplist_t *move = NULL;
+
+
+    if (list == NULL)
+        return (NULL);
+    move = list;
+    myEx = list;
+    while (myEx->express != NULL)
+    {
+        move = myEx;
+        myEx = myEx->express;
+        printf("Value checked at index [%zu] = [%d]\n", myEx->index, myEx->n);
+        if (value <= myEx->n)
+        {
+            break;
+        }
+    }
+    printf("Value found between indexes [%zu] and [%zu]\n", move->index, myEx->index);
+    while (move->index != myEx->index)
+    {
+        printf("Value checked at index [%zu] = [%d]\n", move->index, move->n);
+        if (value <= move->n)
+        {
+            break;
+        }
+        move = move->next;
+    }
+    if (value == move->n)
+    {
+        return (move);
+    }
+    return (NULL);
+}
