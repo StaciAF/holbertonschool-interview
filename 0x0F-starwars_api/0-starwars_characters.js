41cb0cc7a2fe114ff56a40846c3afd351fd7ae9f
StaciAF <aaen.it19@gmail.com>
2021-04-08 17:51:07 -0500

    checks semistandard linter, characters printing random order

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -9,8 +9,6 @@ request(url, 'utf-8', function (err, resp, body) {
   } else {
     const mList = JSON.parse(body);
     for (let i = 0; i < mList.characters.length; i++) {
-      const process2 = require('process');
-      const request2 = require('request');
       const url2 = mList.characters[i];
       request(url2, 'utf-8', function (err2, resp2, body2) {
         if (err2) {
