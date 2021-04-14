dc8a61afe138d5cd5ab7bbb3ebb3c88093e67ca1
StaciAF <aaen.it19@gmail.com>
2021-04-14 17:50:09 -0500

    adds solution to rain algo

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,12 +8,9 @@ def rain(walls):
     """this method calculates how much water
     will be retained then returns amount
     """
-    if not walls:
-        return 0
-    length = (len(walls) - 2)
-    i = 1
-    while i < (length):
-        each = walls[i]
-        each += each
-        i += 1
-    return each
+    amount = 0
+    for i in range(len(walls)):
+        left_max = max(walls[:i + 1])
+        right_max = max(walls[i:])
+        amount += max(min(left_max, right_max) - walls[i], 0)
+    return amount
