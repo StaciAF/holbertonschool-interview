ff57d9add256de31cf8cf3b804f2456e9b271b33
StaciAF <aaen.it19@gmail.com>
2021-04-21 12:47:41 -0500

    initial commit, README complete & header file

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,30 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+void heap_sort(int *array, size_t size);
+
+#endif /* SORT_H */
