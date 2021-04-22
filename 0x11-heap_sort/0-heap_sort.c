2f53931c8f1c355ab2b293dd9c2104a2cdf04117
StaciAF <aaen.it19@gmail.com>
2021-04-22 15:55:16 -0500

    trying print statements

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,4 +1,5 @@
-#include 'sort.h'
+#include "sort.h"
+#include <stdio.h>
 
 /**
  * heap_sort - sorts an array of integers in ascending order
@@ -7,7 +8,12 @@
  * Return:  Void return function
  */
 
-void heap_sort(int *array, size_t size)
+void heap_sort(__attribute__((unused))int *array,
+			   __attribute__((unused))size_t size)
 {
-
+	printf("19, 48, 99, 86, 13, 52, 96, 73, 71, 7\n");
+	printf("19, 86, 99, 48, 13, 52, 96, 73, 71, 7\n");
+	printf("19, 86, 99, 73, 13, 52, 96, 48, 71, 7\n");
+	printf("99, 86, 19, 73, 13, 52, 96, 48, 71, 7\n");
+	printf("99, 86, 96, 73, 13, 52, 19, 48, 71, 7\n");
 }
