2d8d779a55c438faf8f1a9a0343d16c05b75ea0f
StaciAF <aaen.it19@gmail.com>
2021-05-13 19:51:42 -0500

    initial commit, adds README and file structure

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to the root node of the heap
+ * Return: value of root node or 0 upon failure
+ **/
+
+int heap_extract(heap_t **root)
+{
+	if (root == NULL || *root == NULL)
+		return (0);
+}
