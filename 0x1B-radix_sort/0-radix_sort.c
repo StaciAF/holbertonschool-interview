00cde663e2a735512b6b17238f8d0d6e4f6b8a18
StaciAF <aaen.it19@gmail.com>
2021-07-04 14:32:35 -0500

    adds files for solution to Radix Sort algo

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+
+/**
+ * get_max - gets the max
+ * @array: array given
+ * @size: size of given array
+ * Return: max int
+ **/
+int get_max(int *array, size_t size)
+{
+	int max = array[0];
+	int intSize = size;
+
+	for (int i = 1; i < intSize; i++)
+		if (array[i] > max)
+			max = array[i];
+	return (max);
+}
+
+/**
+ * radix_sort - sorts digits LSD radix style
+ * @array: array given
+ * @size: size of given array
+ **/
+void radix_sort(int *array, size_t size)
+{
+	int bucket[10][10], bucket_cnt[10];
+	int i, j, k, r, NOP = 0, divisor = 1, lar, pass;
+	int intSize = size;
+
+	lar = get_max(array, intSize);
+	while (lar > 0)
+	{
+		NOP++;
+		lar /= 10;
+	}
+	for (pass = 0; pass < NOP; pass++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			bucket_cnt[i] = 0;
+		}
+		for (i = 0; i < intSize; i++)
+		{
+			r = (array[i] / divisor) % 10;
+			bucket[r][bucket_cnt[r]] = array[i];
+			bucket_cnt[r] += 1;
+		}
+		i = 0;
+		for (k = 0; k < 10; k++)
+		{
+			for (j = 0; j < bucket_cnt[k]; j++)
+			{
+				array[i] = bucket[k][j];
+				i++;
+			}
+		}
+		print_array(array, size);
+		divisor *= 10;
+	}
+}
---
+++ 0x1B-radix_sort/README.md
@@ -0,0 +1,6 @@
+# 0x1B. Radix Sort
+
+
+### Task
+#### 0. Radix sort
+``` void radix_sort(int *array, size_t size); ```
