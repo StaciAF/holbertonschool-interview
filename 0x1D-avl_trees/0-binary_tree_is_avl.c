59171ba3399d4789e836ffba6b07ef7a8602a2d7
StaciAF <aaen.it19@gmail.com>
2021-07-21 16:45:33 -0500

    initial commit, adds file structure to complete task

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - checks if given binary tree has valid AVL properties
+ * @tree: pointer to root node of binary tree passed to check
+ *
+ * Return: 1 for valid AVL else 0
+ */
+
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+}
